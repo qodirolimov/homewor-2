1e67082a25573d1c232555a599aa5b4745f58dac
qodirolimov <qodirolimov88@gmail.com>
2022-08-12 21:14:57 +0500

    uyga vazifa

---
+++ script.js
@@ -0,0 +1,91 @@
+// "use strict"
+
+
+      // 1-dars TIP lar  bo'limi  
+
+               // 1-masala
+// let  sm=100;
+// let  mm= prompt  ("santimetr yozing metr.sm chiqadi boshida 0 yozmang ");
+
+// let metrValue =  (  mm / sm );
+// console.log(`${metrValue}metr`);
+
+
+               // 2-masala
+// let uzum=1000;
+// let son= prompt ("son yozing kg) (tonna chiqadi  boshida 0 yozmang");
+// let tonna = (son / uzum);
+
+// console.log(`${tonna}t`);
+
+
+
+
+
+// 1-dars Mantiqiy amallar (if, else)
+
+               //   1-masala
+
+// let num=8;
+
+// if (num < 9.4  ){
+//    console.log("yaxshi");
+// }else{
+//    console.log('xato');
+// }
+
+
+
+
+            // 4-masala
+
+// let a=17;
+// let b=12;
+// let c=16;
+
+
+//  if (  a > b  && b > c  ){
+//    console.log("to'g'ri");
+//  }else{
+//    console.log("xato");
+//  }
+ 
+
+
+//             5-masala
+// let a=17;
+// let b=12;
+// let c=16;
+
+
+//  if (  a > b  && b > c  ){
+//    console.log("to'g'ri");
+//  }else{
+//    console.log("xato");
+//  }
+ 
+
+
+
+               // 14-masala
+
+// let num=21;   
+// let   d=41;
+
+// if ( num > d ){
+//       console.log("true");
+// }else{
+//       console.log('false');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
